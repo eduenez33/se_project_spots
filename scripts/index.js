779433ffe41baf160f7a40ecf6e2e6b05ab1a1d3
Edwin Duenez <duenezedwin33@gmail.com>
2025-03-06 11:28:12 -0700

    Create a script file
    Create an array with 6 objects. Each object should contain 2 fields, a name and a link to an image.
    Link the script file to index.html

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Santa Barbara",
+    link: "https://unsplash.com/photos/people-walking-on-beach-during-daytime-qN2UhvqAU3Y",
+  },
+  {
+    name: "Austin",
+    link: "https://unsplash.com/photos/black-and-red-unks-building-pbrvz7emGDE",
+  },
+  {
+    name: "Salt Lake City",
+    link: "https://unsplash.com/photos/parked-cars-in-the-middle-of-city-with-high-rise-buildings-jozagDIvslM",
+  },
+  {
+    name: "Ventura",
+    link: "https://unsplash.com/photos/an-overhead-view-of-a-city-street-with-buildings-hNkrzwuaxZg",
+  },
+  {
+    name: "Dallas",
+    link: "https://unsplash.com/photos/a-car-parked-on-the-side-of-the-road-at-night-IK4vMYFnzZA",
+  },
+  {
+    name: "San Luis Obispo",
+    link: "https://unsplash.com/photos/photo-of-mountain-and-city-scenery-mHDdwWwoVvw",
+  },
+];
