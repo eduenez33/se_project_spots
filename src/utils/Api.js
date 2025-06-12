78061fe92ec652aea621fd6186000b18d8f81bfd
Edwin Duenez <duenezedwin33@gmail.com>
2025-06-12 13:09:53 -0600

    add method to update user avatar via PATCH request

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -37,6 +37,16 @@ class Api {
       }),
     }).then(this._checkResponse);
   }
+
+  editUserAvatar({ avatar }) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar,
+      }),
+    }).then(this._checkResponse);
+  }
 }
 
 export default Api;
