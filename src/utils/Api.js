c24e1ad842b270297b14b045cd08521375dde674
Edwin Duenez <duenezedwin33@gmail.com>
2025-06-11 23:28:28 -0600

    add Api class with methods to fetch initial cards and user info

---
+++ src/utils/Api.js
@@ -0,0 +1,27 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+}
+
+export default Api;
