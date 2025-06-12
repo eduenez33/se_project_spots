28c3dcf171d257c3f243d73e02450dc625341fff
Edwin Duenez <duenezedwin33@gmail.com>
2025-06-12 11:29:15 -0600

    refactor image handling by requiring icons directly in HTML

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,10 @@ import {
   settings,
 } from "../scripts/validation.js";
 import avatarSrc from "../images/avatar.jpg";
-import pencilSrc from "../images/icon_pencil.svg";
-import plusSrc from "../images/icon_plus.svg";
 import Api from "../utils/Api.js";
 
 const spotsAvatar = document.getElementById("profile-avatar");
 spotsAvatar.src = avatarSrc;
-const spotsPencilIcon = document.getElementById("pencil-icon");
-spotsPencilIcon.src = pencilSrc;
-const spotsPlusIcon = document.getElementById("plus-icon");
-spotsPlusIcon.src = plusSrc;
 
 const cardList = document.querySelector(".cards__list");
 const profileEditButton = document.querySelector(".profile__edit-button");
